518f54f9030ca9149d8aabc57d23d6a3d744eb9d
luisoft94 <luisperera_94@hotmail.com>
2017-03-29 21:29:01 -0400

    Add files via upload

---
+++ Fase 3/eliminarbd.sql
@@ -0,0 +1,51 @@
+
+/*Tablas de Nivel 3*/
+DROP TABLE m.Incidente;
+DROP TABLE m.Financiador_comp;
+
+
+/*Tablas de Nivel 2*/
+DROP TABLE m.Beca;
+DROP TABLE m.Conforman;
+DROP TABLE m.Viaja;
+DROP TABLE m.Participa;
+DROP TABLE m.Posee;
+DROP TABLE m.Resuelve;
+
+/*Tablas de Nivel 1*/
+DROP TABLE m.Persona;
+DROP TABLE m.Equipo;
+DROP TABLE m.Competencia;
+DROP TABLE m.Site;
+DROP TABLE m.Problema;
+
+
+
+DROP DOMAIN m.veinte_caracteres;
+DROP DOMAIN m.ochenta_caracteres;
+DROP DOMAIN m.diez_mil_caracteres;
+DROP DOMAIN m.tipo_persona;
+DROP DOMAIN m.nivel;
+DROP DOMAIN m.dificultad;
+DROP DOMAIN m.estatus;
+DROP DOMAIN m.entero_mayor_igual_cero;
+DROP DOMAIN m.entero_mayor_cero;
+
+
+
+/*
+REVOKE ALL ON SCHEMA ERP FROM admin;
+REVOKE ALL ON SCHEMA ERP FROM empleado;
+
+DROP USER trabajador;
+DROP USER administrador;
+
+DROP ROLE admin;
+DROP ROLE empleado;*/
+
+
+DROP SCHEMA m;
+
+\c postgres
+
+DROP DATABASE proyecto;
